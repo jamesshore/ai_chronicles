8dbbb0bd8a1138b10b1e8ccc8cf596abe725ae7a
James Shore <jshore@jamesshore.com>
2023-04-28 17:44:25 -0700

    Adjust repo branch configuration

--- build/config/branches.js
+++ build/config/branches.js
@@ -1,28 +1,6 @@
 // Copyright Titanium I.T. LLC.
 
 export default {
-	integration: "javascript",
-	typescript: "typescript",
+	integration: "integration",
 	dev: "dev",
-
-	// training course exercises
-	usingNullablesJs: "using-nullables-js",
-	usingNullablesJsValidation: "using-nullables-js-validate",
-	usingNullablesTs: "using-nullables-ts",
-	usingNullablesTsValidation: "using-nullables-ts-validate",
-
-	highLevelInfrastructureJs: "high-level-infrastructure-js",
-	highLevelInfrastructureJsValidation: "high-level-infrastructure-js-validate",
-	highLevelInfrastructureTs: "high-level-infrastructure-ts",
-	highLevelInfrastructureTsValidation: "high-level-infrastructure-ts-validate",
-
-	narrowIntegrationTestsJs: "narrow-integration-tests-js",
-	narrowIntegrationTestsJsValidation: "narrow-integration-tests-js-validate",
-	narrowIntegrationTestsTs: "narrow-integration-tests-ts",
-	narrowIntegrationTestsTsValidation: "narrow-integration-tests-ts-validate",
-
-	embeddedStubsJs: "embedded-stubs-js",
-	embeddedStubsJsValidation: "embedded-stubs-js-validate",
-	embeddedStubsTs: "embedded-stubs-ts",
-	embeddedStubsTsValidation: "embedded-stubs-ts-validate",
 };
