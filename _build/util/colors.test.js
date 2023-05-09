924d9d0b001ba963545db7ae5a5caa1ab21a1e7a
James Shore <jshore@jamesshore.com>
2023-05-08 17:38:47 -0700

    Add command-line runner for Mocha

--- _build/util/colors.test.js
+++ _build/util/colors.test.js
@@ -8,15 +8,15 @@ describe("Colors", () => {
 	const { red } = colors;    // see production code for other supported colors
 
 	it("color-codes text", () => {
-		expect(red("text")).toBe("\u001b[31mtext\u001b[0mx");		// FAIL
-		// expect(red("text")).toBe("\u001b[31mtext\u001b[0mx");	// PASS
+		// assert.equal(red("text"), "\u001b[31mtext\u001b[0mx");		// FAIL
+		assert.equal(red("text"), "\u001b[31mtext\u001b[0m");			// PASS
 	});
 
 	it("has styling", () => {
 		// note that support for styles depends on terminal emulator
 
-		// expect(red.bold("text")).toBe("\u001b[1;31mtext\u001b[0mX");	// FAIL
-		expect(red.bold("text")).toBe("\u001b[1;31mtext\u001b[0m");		// PASS
+		// assert.equal(red.bold("text"), "\u001b[1;31mtext\u001b[0mx");		// FAIL
+		assert.equal(red.bold("text"), "\u001b[1;31mtext\u001b[0m");		// PASS
 
 		assert.equal(red.dim("text"), "\u001b[2;31mtext\u001b[0m", "dim");
 		assert.equal(red.underline("text"), "\u001b[4;31mtext\u001b[0m", "underline");
---
+++ mocha.sh
@@ -0,0 +1,2 @@
+cd _build
+node_modules/.bin/mocha --exclude "node_modules/**/*" "**/*.test.js"
