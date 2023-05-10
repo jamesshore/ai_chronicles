6def1c319eeb2e8c8dddf0689b8653434cb307b8
James Shore <jshore@jamesshore.com>
2023-05-09 19:12:54 -0700

    Tests module re-exports Mocha before/after functions

--- _build/util/tests.js
+++ _build/util/tests.js
@@ -3,3 +3,7 @@ export * as assert from "./assert.js";
 // Globals created by Mocha
 export const describe = global.describe;
 export const it = global.it;
+export const before = global.before;
+export const after = global.after;
+export const beforeEach = global.beforeEach;
+export const afterEach = global.afterEach;
