40e2e72322a90df591f1256a4f179a46a67298ae
James Shore <jshore@jamesshore.com>
2023-05-14 12:08:04 -0700

    Add default greeting

--- front_end/src/index.tsx
+++ front_end/src/index.tsx
@@ -6,4 +6,8 @@ const domNode = document.getElementById('react-root');
 if (domNode === null) throw new Error("Couldn't find 'react-root' DOM element");
 
 const root = createRoot(domNode);
-root.render(<Hello />);
+root.render(<Hello name="fellow meat puppet" onClick={onClick} />);
+
+function onClick() {
+  alert("I too enjoy using my human legs to walk adjacent to dihydrogen monoxide.");
+}
