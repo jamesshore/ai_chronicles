116edc6d7e229efbe25bfdfd8284d3cd95238799
James Shore <jshore@jamesshore.com>
2023-05-15 14:53:30 -0700

    Reformat code

--- back_end/src/test/java/dev/ted/stream/ai_chronicles/infrastructure/JsonHttpClientTest.java
+++ back_end/src/test/java/dev/ted/stream/ai_chronicles/infrastructure/JsonHttpClientTest.java
@@ -3,7 +3,6 @@ package dev.ted.stream.ai_chronicles.infrastructure;
 import dev.ted.stream.ai_chronicles.OutputTracker;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -13,142 +12,142 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JsonHttpClientTest {
 
-    @Test
-    void nulledGetThrowsNotFoundExceptionForUnconfiguredEndpoints() {
-        JsonHttpClient jsonHttpClient = JsonHttpClient.createNull();
+  @Test
+  void nulledGetThrowsNotFoundExceptionForUnconfiguredEndpoints() {
+    JsonHttpClient jsonHttpClient = JsonHttpClient.createNull();
 
-        assertThatThrownBy(() -> {
-            jsonHttpClient.get("/unconfigured", ExampleDto.class);
-        }).isInstanceOf(NoSuchElementException.class)
-          .hasMessage("URL not configured: /unconfigured");
+    assertThatThrownBy(() -> {
+      jsonHttpClient.get("/unconfigured", ExampleDto.class);
+    }).isInstanceOf(NoSuchElementException.class)
+      .hasMessage("URL not configured: /unconfigured");
+  }
+
+  @Test
+  void nulledGetReturnsSingleConfiguredInstanceForever() {
+    ExampleDto configuredExampleDto = new ExampleDto("configured value");
+
+    JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
+      Map.of("/configured", configuredExampleDto)
+    );
+
+    ExampleDto exampleDto1 = jsonHttpClient.get("/configured", ExampleDto.class);
+    assertThat(exampleDto1.getContent())
+      .isEqualTo("configured value");
+
+    ExampleDto exampleDto2 = jsonHttpClient.get("/configured", ExampleDto.class);
+    assertThat(exampleDto2.getContent())
+      .isEqualTo("configured value");
+
+    ExampleDto exampleDto3 = jsonHttpClient.get("/configured", ExampleDto.class);
+    assertThat(exampleDto3.getContent())
+      .isEqualTo("configured value");
+  }
+
+  @Test
+  void nullGetReturnsDifferentConfiguredInstancesWhenGivenList() {
+    JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
+      Map.of("/configured-list", List.of(
+        new ExampleDto("dto 1"),
+        new ExampleDto("dto 2"),
+        new ExampleDto("dto 3"))));
+
+
+    ExampleDto exampleDto1 = jsonHttpClient.get("/configured-list",
+      ExampleDto.class);
+    assertThat(exampleDto1.getContent())
+      .isEqualTo("dto 1");
+
+    ExampleDto exampleDto2 = jsonHttpClient.get("/configured-list",
+      ExampleDto.class);
+    assertThat(exampleDto2.getContent())
+      .isEqualTo("dto 2");
+
+    ExampleDto exampleDto3 = jsonHttpClient.get("/configured-list",
+      ExampleDto.class);
+    assertThat(exampleDto3.getContent())
+      .isEqualTo("dto 3");
+  }
+
+  @Test
+  void nullGetThrowsExceptionWhenListOfConfiguredInstancesRunsOut() {
+    JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
+      Map.of("/list-of-one?a=b", List.of(new ExampleDto("dto 1")))
+    );
+    jsonHttpClient.get("/list-of-one?a={parm}", ExampleDto.class, "b");
+
+    assertThatThrownBy(() -> jsonHttpClient.get("/list-of-one?a={parm}", ExampleDto.class, "b"))
+      .isInstanceOf(NoSuchElementException.class)
+      .hasMessage("No more responses configured for URL: /list-of-one?a=b");
+  }
+
+  @Test
+  void nulledGetReturnsDifferentValuesBasedOnUriAndParameters() {
+    ExampleDto configuredExampleDto1a = new ExampleDto("configured 1a");
+    ExampleDto configuredExampleDto1b = new ExampleDto("configured 1b");
+    ExampleDto configuredExampleDto2 = new ExampleDto("configured 2");
+
+    JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(Map.of(
+      "/configured1?parm=a", configuredExampleDto1a,
+      "/configured1?parm=b", configuredExampleDto1b,
+      "/configured2", configuredExampleDto2)
+    );
+
+    ExampleDto exampleDto1a = jsonHttpClient.get("/configured1?parm={first}",
+      ExampleDto.class,
+      "a");
+    assertThat(exampleDto1a.getContent())
+      .isEqualTo("configured 1a");
+
+    ExampleDto exampleDto1b = jsonHttpClient.get("/configured1?parm={first}",
+      ExampleDto.class,
+      "b");
+    assertThat(exampleDto1b.getContent())
+      .isEqualTo("configured 1b");
+
+    ExampleDto exampleDto2 = jsonHttpClient.get("/configured2", ExampleDto.class);
+    assertThat(exampleDto2.getContent())
+      .isEqualTo("configured 2");
+  }
+
+  @Test
+  void requestsAreTracked() {
+    JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
+      Map.of("/get-endpoint?a", new ExampleDto())
+    );
+
+    OutputTracker<JsonHttpRequest> tracker = jsonHttpClient.trackRequests();
+
+    jsonHttpClient.get("/get-endpoint?{parm}", ExampleDto.class, "a");
+
+    Map<String, String> headers = Map.of("header1", "value1", "header2", "value2");
+    ExampleDto postedBody = new ExampleDto("post");
+    jsonHttpClient.post("/post-endpoint", headers, postedBody);
+
+    assertThat(tracker.output())
+      .containsExactly(
+        JsonHttpRequest.createGet("/get-endpoint?a"),
+        JsonHttpRequest.createPost("/post-endpoint", headers, postedBody));
+  }
+
+
+  public static class ExampleDto {
+    private String content;
+
+    public ExampleDto() {
+      content = "initial value";
     }
 
-    @Test
-    void nulledGetReturnsSingleConfiguredInstanceForever() {
-        ExampleDto configuredExampleDto = new ExampleDto("configured value");
-
-        JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
-                Map.of("/configured", configuredExampleDto)
-        );
-
-        ExampleDto exampleDto1 = jsonHttpClient.get("/configured", ExampleDto.class);
-        assertThat(exampleDto1.getContent())
-                .isEqualTo("configured value");
-
-        ExampleDto exampleDto2 = jsonHttpClient.get("/configured", ExampleDto.class);
-        assertThat(exampleDto2.getContent())
-                .isEqualTo("configured value");
-
-        ExampleDto exampleDto3 = jsonHttpClient.get("/configured", ExampleDto.class);
-        assertThat(exampleDto3.getContent())
-                .isEqualTo("configured value");
+    public ExampleDto(String configuredValue) {
+      content = configuredValue;
     }
 
-    @Test
-    void nullGetReturnsDifferentConfiguredInstancesWhenGivenList() {
-        JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
-                Map.of("/configured-list", List.of(
-                        new ExampleDto("dto 1"),
-                        new ExampleDto("dto 2"),
-                        new ExampleDto("dto 3"))));
-
-
-        ExampleDto exampleDto1 = jsonHttpClient.get("/configured-list",
-                                                    ExampleDto.class);
-        assertThat(exampleDto1.getContent())
-                .isEqualTo("dto 1");
-
-        ExampleDto exampleDto2 = jsonHttpClient.get("/configured-list",
-                                                    ExampleDto.class);
-        assertThat(exampleDto2.getContent())
-                .isEqualTo("dto 2");
-
-        ExampleDto exampleDto3 = jsonHttpClient.get("/configured-list",
-                                                    ExampleDto.class);
-        assertThat(exampleDto3.getContent())
-                .isEqualTo("dto 3");
+    public String getContent() {
+      return content;
     }
 
-    @Test
-    void nullGetThrowsExceptionWhenListOfConfiguredInstancesRunsOut() {
-        JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
-                Map.of("/list-of-one?a=b", List.of(new ExampleDto("dto 1")))
-        );
-        jsonHttpClient.get("/list-of-one?a={parm}", ExampleDto.class, "b");
-
-        assertThatThrownBy(() -> jsonHttpClient.get("/list-of-one?a={parm}", ExampleDto.class, "b"))
-                .isInstanceOf(NoSuchElementException.class)
-                .hasMessage("No more responses configured for URL: /list-of-one?a=b");
+    public void setContent(String content) {
+      this.content = content;
     }
-
-    @Test
-    void nulledGetReturnsDifferentValuesBasedOnUriAndParameters() {
-        ExampleDto configuredExampleDto1a = new ExampleDto("configured 1a");
-        ExampleDto configuredExampleDto1b = new ExampleDto("configured 1b");
-        ExampleDto configuredExampleDto2 = new ExampleDto("configured 2");
-
-        JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(Map.of(
-                "/configured1?parm=a", configuredExampleDto1a,
-                "/configured1?parm=b", configuredExampleDto1b,
-                "/configured2", configuredExampleDto2)
-        );
-
-        ExampleDto exampleDto1a = jsonHttpClient.get("/configured1?parm={first}",
-                                                     ExampleDto.class,
-                                                     "a");
-        assertThat(exampleDto1a.getContent())
-                .isEqualTo("configured 1a");
-
-        ExampleDto exampleDto1b = jsonHttpClient.get("/configured1?parm={first}",
-                                                     ExampleDto.class,
-                                                     "b");
-        assertThat(exampleDto1b.getContent())
-                .isEqualTo("configured 1b");
-
-        ExampleDto exampleDto2 = jsonHttpClient.get("/configured2", ExampleDto.class);
-        assertThat(exampleDto2.getContent())
-                .isEqualTo("configured 2");
-    }
-
-    @Test
-    void requestsAreTracked() {
-        JsonHttpClient jsonHttpClient = JsonHttpClient.createNull(
-                Map.of("/get-endpoint?a", new ExampleDto())
-        );
-
-        OutputTracker<JsonHttpRequest> tracker = jsonHttpClient.trackRequests();
-
-        jsonHttpClient.get("/get-endpoint?{parm}", ExampleDto.class, "a");
-
-        Map<String, String> headers = Map.of("header1", "value1", "header2", "value2");
-        ExampleDto postedBody = new ExampleDto("post");
-        jsonHttpClient.post("/post-endpoint", headers, postedBody);
-
-        assertThat(tracker.output())
-                .containsExactly(
-                        JsonHttpRequest.createGet("/get-endpoint?a"),
-                        JsonHttpRequest.createPost("/post-endpoint", headers, postedBody));
-    }
-
-
-    public static class ExampleDto {
-        private String content;
-
-        public ExampleDto() {
-            content = "initial value";
-        }
-
-        public ExampleDto(String configuredValue) {
-            content = configuredValue;
-        }
-
-        public String getContent() {
-            return content;
-        }
-
-        public void setContent(String content) {
-            this.content = content;
-        }
-    }
+  }
 
 }
--- back_end/src/test/java/dev/ted/stream/ai_chronicles/infrastructure/OpenAiClientTest.java
+++ back_end/src/test/java/dev/ted/stream/ai_chronicles/infrastructure/OpenAiClientTest.java
@@ -9,35 +9,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 class OpenAiClientTest {
-    // Test 0:
-    // Manually figure out what the request and responses actually are
+  // Test 0:
+  // Manually figure out what the request and responses actually are
 
-    @Test
-    void sendsPromptToOpenAi() {
-        JsonHttpClient httpClient = JsonHttpClient.createNull();
-        var httpRequests = httpClient.trackRequests();
+  @Test
+  void sendsPromptToOpenAi() {
+    JsonHttpClient httpClient = JsonHttpClient.createNull();
+    var httpRequests = httpClient.trackRequests();
 
-        OpenAiClient openAi = new OpenAiClient(httpClient, "my_api_key");
-        openAi.prompt("my_prompt");
+    OpenAiClient openAi = new OpenAiClient(httpClient, "my_api_key");
+    openAi.prompt("my_prompt");
 
-        JsonHttpRequest expectedRequest = JsonHttpRequest.createPost(
-                "https://api.openai.com/v1/chat/completions",
-                Map.of(
-                        "Authorization", "Bearer my_api_key",
-                        "Content-Type", "application/json"
-                ),
-                new PromptBody("gpt-3.5-turbo",
-                        List.of(new PromptBody.Message("user", "my_prompt")),
-                        0.7
-                )
-        );
-        assertThat(httpRequests.output()).containsExactly(expectedRequest);
+    JsonHttpRequest expectedRequest = JsonHttpRequest.createPost(
+      "https://api.openai.com/v1/chat/completions",
+      Map.of(
+        "Authorization", "Bearer my_api_key",
+        "Content-Type", "application/json"
+      ),
+      new PromptBody("gpt-3.5-turbo",
+        List.of(new PromptBody.Message("user", "my_prompt")),
+        0.7
+      )
+    );
+    assertThat(httpRequests.output()).containsExactly(expectedRequest);
 
 
-
-        // a. Create a Nulled HttpClient
-        // b. Make the request
-        // c. Check that the correct request was made
+    // a. Create a Nulled HttpClient
+    // b. Make the request
+    // c. Check that the correct request was made
 
 //        POST https://api.openai.com/v1/chat/completions
 //        Authorization: Bearer {{openai}}
@@ -55,18 +54,18 @@ class OpenAiClientTest {
 //        }
 
 
-    }
+  }
 
-    @Test
-    void parsesResponseFromOpenAi() {
-        // Test 2: Parses the HTTP response
-        // a. Create a Nulled HttpClient with a specific response hardcoded
-        // b. Make the request
-        // c. Check the return value to see that the response was parsed correctly
+  @Test
+  void parsesResponseFromOpenAi() {
+    // Test 2: Parses the HTTP response
+    // a. Create a Nulled HttpClient with a specific response hardcoded
+    // b. Make the request
+    // c. Check the return value to see that the response was parsed correctly
 
 
-        // Test 3..n: Error handling
+    // Test 3..n: Error handling
 
-        // Including Paranoic telemetry
-    }
+    // Including Paranoic telemetry
+  }
 }
