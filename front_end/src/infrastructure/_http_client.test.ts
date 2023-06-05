e5f32e4d4aa578a87c0502382633e2a6a53846ab
James Shore <jshore@jamesshore.com>
2023-06-05 15:24:13 -0700

    SpyServer destroys open sockets with extreme prejudice

--- front_end/src/infrastructure/_http_client.test.ts
+++ front_end/src/infrastructure/_http_client.test.ts
@@ -1,6 +1,7 @@
 import { assert, test } from "../util/tests.js";
 import { HttpClient } from "./http_client.js";
 import * as http from "node:http";
+import * as net from "net";
 
 
 const PORT = 5011;
@@ -26,8 +27,46 @@ export default test(({ describe, it, beforeAll, beforeEach, afterAll }) => {
       status: 201,
       headers: {
         myResponseHeader: "header-value",
-        // prevent client from keeping connection open and preventing the server from stopping
-        connection: "close",
+      },
+      body: "my_response_body",
+    });
+
+
+    const client = new HttpClient();
+    await client.requestAsync({
+      url: `http://localhost:${PORT}/my-path`,
+      method: "post",
+      headers: {
+        "my-header": "my-value",
+      },
+      body: "my_request_body",
+    });
+
+    assert.deepEqual(server.lastRequest, {
+      path: "/my-path",
+      method: "POST",
+      headers: {
+        "my-header": "my-value",
+        // fetch() adds a bunch of headers that we don't explicitly define
+        "host": `localhost:${PORT}`,
+        "connection": "keep-alive",
+        "content-type": "text/plain;charset=UTF-8",
+        "accept": "*/*",
+        "accept-language": "*",
+        "sec-fetch-mode": "cors",
+        "user-agent": "undici",
+        "accept-encoding": "gzip, deflate",
+        "content-length": "15",
+      },
+      body: "my_request_body",
+    });
+  });
+
+  it.skip("makes HTTP requests", async () => {
+    server.setResponse({
+      status: 201,
+      headers: {
+        myResponseHeader: "header-value",
       },
       body: "my_response_body",
     });
@@ -75,8 +114,6 @@ export default test(({ describe, it, beforeAll, beforeEach, afterAll }) => {
 });
 
 
-
-
 interface SpyServerRequest {
   path?: string,
   method?: string,
@@ -95,6 +132,7 @@ class SpyServer {
   private readonly _httpServer;
   private _lastRequest!: null | SpyServerRequest;
   private _nextResponse!: SpyServerResponse;
+  private _connections: net.Socket[] = [];
 
   constructor() {
     this._httpServer = http.createServer();
@@ -115,6 +153,8 @@ class SpyServer {
     headers: {},
     body: ""
   }) {
+    console.log({ response });
+
     this._nextResponse = response;
   }
 
@@ -123,6 +163,9 @@ class SpyServer {
       this._httpServer.listen(PORT);
       this._httpServer.on("listening", () => {
         resolve();
+      });
+      this._httpServer.on("connection", (socket: net.Socket) => {
+        this._connections.push(socket);
       });
       this._httpServer.on("request", (request: http.IncomingMessage, response: http.ServerResponse) => {
         let body = "";
@@ -151,6 +194,9 @@ class SpyServer {
   async stopAsync() {
     await new Promise<void>((resolve, reject) => {
       this._httpServer.close();
+      this._connections.forEach((socket) => {
+        socket.destroy();
+      });
       this._httpServer.on("close", () => {
         resolve();
       });
