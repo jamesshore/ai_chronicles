58268ea56f5b6513760c8abd9c0ca4e8ee8f88d1
James Shore <jshore@jamesshore.com>
2023-06-26 15:47:32 -0700

    Clean up BackEndClient tests

--- front_end/src/infrastructure/_back_end_client.test.ts
+++ front_end/src/infrastructure/_back_end_client.test.ts
@@ -2,28 +2,10 @@ import { assert, test } from "../util/tests.js";
 import { HttpClient } from "./http_client.js";
 import { BackEndClient } from "./back_end_client.js";
 
-const VALID_RESPONSE = {
-  status: 200,
-  headers: {
-    "content-type": "application/json",
-  },
-  body: JSON.stringify({
-    answer: "back_end_answer",
-  }),
-};
-
-const IRRELEVANT_SAY = "irrelevant_say";
-
 export default test(({ it }) => {
 
   it("sends requests", async () => {
-    const httpClient = HttpClient.createNull({
-      "http://localhost:5020/say": VALID_RESPONSE
-    });
-    const httpRequests = httpClient.trackRequests();
-
-    const backEndClient = new BackEndClient(httpClient);
-    await backEndClient.sayAsync("my_message");
+    const { httpRequests } = await sayAsync({ message: "my_message" });
 
     assert.deepEqual(httpRequests.data, [{
       url: "http://localhost:5020/say",
@@ -38,14 +20,30 @@ export default test(({ it }) => {
   });
 
   it("parses responses", async () => {
-    const httpClient = HttpClient.createNull({
-      "http://localhost:5020/say": VALID_RESPONSE
-    });
-
-    const backEndClient = new BackEndClient(httpClient);
-    const answer = await backEndClient.sayAsync(IRRELEVANT_SAY);
-
+    const { answer } = await sayAsync({ configuredAnswer: "back_end_answer" });
     assert.equal(answer, "back_end_answer");
   });
 
 });
+
+async function sayAsync({
+  message = "irrelevant_message",
+  configuredAnswer = "irrelevant_configured_answer",
+}) {
+  const httpClient = HttpClient.createNull({
+    "http://localhost:5020/say": {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        answer: configuredAnswer,
+      }),
+    },
+  });
+  const httpRequests = httpClient.trackRequests();
+
+  const backEndClient = new BackEndClient(httpClient);
+  const answer = await backEndClient.sayAsync(message);
+  return { httpRequests, answer };
+}
