10a5d57434c3ce9f98e5a8738a39a010af8f3483
James Shore <jshore@jamesshore.com>
2023-06-26 13:58:26 -0700

    Test BackEndClient.say() response parsing (not implemented yet)

--- front_end/src/infrastructure/_back_end_client.test.ts
+++ front_end/src/infrastructure/_back_end_client.test.ts
@@ -2,6 +2,8 @@ import { assert, test } from "../util/tests.js";
 import { HttpClient } from "./http_client.js";
 import { BackEndClient } from "./back_end_client.js";
 
+const IRRELEVANT_SAY = "irrelevant_say";
+
 export default test(({ it }) => {
 
   it("sends requests", async () => {
@@ -23,8 +25,23 @@ export default test(({ it }) => {
     }]);
   });
 
-  it("parses responses", () => {
-    assert.todo();
-  });
+  // it("parses responses", async () => {
+  //   const httpClient = HttpClient.createNull({
+  //     "/say": {
+  //       status: 200,
+  //       headers: {
+  //         "content-type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         answer: "back_end_answer",
+  //       }),
+  //     }
+  //   });
+  //
+  //   const backEndClient = new BackEndClient(httpClient);
+  //   const answer = await backEndClient.sayAsync(IRRELEVANT_SAY);
+  //
+  //   assert.equal(answer, "back_end_answer");
+  // });
 
 });
