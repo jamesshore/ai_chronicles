0f113671036b4198bcf706c440b86fad396f9e97
James Shore <jshore@jamesshore.com>
2023-07-03 15:11:15 -0700

    Back-end /say endpoint returns hardcoded response

--- back_end/src/test/java/dev/ted/stream/ai_chronicles/application/SayControllerWebTest.java
+++ back_end/src/test/java/dev/ted/stream/ai_chronicles/application/SayControllerWebTest.java
@@ -1,9 +1,12 @@
 package dev.ted.stream.ai_chronicles.application;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -18,24 +21,24 @@ public class SayControllerWebTest {
   MockMvc mockMvc;
 
   @Test
-  void acceptsPostRequests() throws Exception {
-    mockMvc.perform(post("/say"))
-      .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-  }
-
-  @Test
-  void returnsResponse() throws Exception {
-    MvcResult result = mockMvc.perform(post("/say"))
-      .andExpect(MockMvcResultMatchers.status().isOk())
+  void acceptsPostAndReturnsResponse() throws Exception {
+    MvcResult result = mockMvc.perform(post("/say")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content("""
+          {"message": "irrelevant_message"}"""))
       .andReturn();
+    MockHttpServletResponse response = result.getResponse();
 
-    assertThat(result.getResponse().getHeader("content-type"))
+    assertThat(response.getStatus())
+      .isEqualTo(200);
+
+    assertThat(response.getHeader("content-type"))
       .isEqualTo("application/json");
 
     JSONAssert.assertEquals("""
-        { "answer": "hardcoded_answer" }
-      """,
-      result.getResponse().getContentAsString(), false);
+          { "answer": "hardcoded_answer" }
+        """,
+      response.getContentAsString(), false);
   }
 
 }
